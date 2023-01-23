0b12401ffb2b2e5232c8dac4daae8b5ee985f899
ION606 <itamarorenn@protonmail.com>
2023-01-22 20:47:31 -0500

    Updated the way I store encryption keys

--- commands/utils/encryption.js
+++ commands/utils/encryption.js
@@ -1,14 +1,14 @@
 // Nodejs encryption with CTR
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32);
+const key = Buffer.from(require('../../config.json').encKey, 'hex');
 const iv = crypto.randomBytes(16);
 
 
 function encrypt(text) {
     return new Promise((resolve, reject) => {
         try {
-            let cipher = crypto.createCipheriv(algorithm,Buffer.from(key), iv);
+            let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
             let encrypted = cipher.update(text);
             encrypted = Buffer.concat([encrypted, cipher.final()]);
             resolve({ iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') });
@@ -17,6 +17,7 @@ function encrypt(text) {
         }
     });
 }
+
 
 function decrypt(text) {
     return new Promise((resolve, reject) => {
